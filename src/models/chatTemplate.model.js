553574316633d13d7d222685b18ae07be4edf291
Andresluna93 <andresucsg93@gmail.com>
2026-08-26 17:03:23 -0500

    update chattemplatemodel new parameter name

--- src/models/chatTemplate.model.js
+++ src/models/chatTemplate.model.js
@@ -1,20 +1,24 @@
 import mongoose from "mongoose";
 
-const chatTemplateSchema = new mongoose.Schema({
+const chatTemplateSchema = new mongoose.Schema(
+  {
     wa_id: { type: String, required: true, index: true },
+    name: { type: String, required: true, index: true },
     client_text: { type: String, required: true },
-    ia_text: {type: String, required: true},
+    ia_text: { type: String, required: true },
     tipo: {
       type: String,
       enum: ["text", "image", "audio", "document", "sticker"],
       default: "text",
     },
-    status:{type: String, default:"interactuaron"},
+    status: { type: String, default: "interactuaron" },
     fecha: { type: Date, default: Date.now },
-},{
+  },
+  {
     timestamps: true,
     versionKey: false,
-  })
+  },
+);
 
-  const ChatTemplate = mongoose.model("ChatTemplate",chatTemplateSchema)
-  export default ChatTemplate;
+const ChatTemplate = mongoose.model("ChatTemplate", chatTemplateSchema);
+export default ChatTemplate;
